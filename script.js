d3577821141f1d10770e6e7fc904d09ef7be3dbe
Hossayin <mayahossein@Hossayins-MacBook-Air.local>
2019-09-29 19:40:11 -0400

    actually saved the progress

--- script.js
+++ script.js
@@ -6,3 +6,16 @@
 // >=
 // >
 // <
+let eventType = prompt("What kind of event are you attending?");
+let tempature = prompt("What will be the tempature during the event?");
+let eventResponse;
+let tempResponse;
+// how can I put both, (event type === "formal" "Formal")
+// why will eventResponse have more than one condition?and event type
+if (eventType === "casual") {
+  eventResponse = "somthing comfy";
+} else if (eventType === "semi-formal") {
+  eventResponse = "a polo";
+} else if (eventType === "formal") {
+  eventResponse = "a suit";
+}
